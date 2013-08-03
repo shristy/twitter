1a105bf06771b60dd72c534c7f777512e2274ebf
shristy <agarwal.shristy1@gmail.com>
2013-08-03 12:14:36 +0530

    My Message

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :person, :message, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter::Application.config.secret_key_base = '8c568fd20e5ad6cd85adabe3abe392c8b3ef90b54599afe0537dd5e019e13845ca781deb3f9cec75226734305b69e961abdaa505a3772fac3c858b7def6737d5'
